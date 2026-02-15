1cddb769a7cdba96e9fad96cad5ef1da5ce0e2c4
Ante Wall <antewall@gmail.com>
2026-02-15 20:28:27 +0100

    fixed git commands

--- nvim/lua/plugins/fugitive.lua
+++ nvim/lua/plugins/fugitive.lua
@@ -2,6 +2,13 @@ return {
 	"tpope/vim-fugitive",
 	config = function()
 		vim.keymap.set("n", "<leader>gs", vim.cmd.Git, { desc = "Git status" })
+		vim.keymap.set("n", "<leader>gp", "<cmd>Git push<CR>", { desc = "Git push" })
+		vim.keymap.set("n", "<leader>gP", "<cmd>Git pull --rebase<CR>", { desc = "Git pull rebase" })
+		vim.keymap.set("n", "<leader>gt", ":Git push -u origin ", { desc = "Git push set upstream" })
+		vim.keymap.set("n", "<leader>gA", "<cmd>Git add -A<CR>", { desc = "Git add all" })
+		vim.keymap.set("n", "<leader>ga", ":Git add ", { desc = "Git add (prompt)" })
+		vim.keymap.set("n", "<leader>gc", "<cmd>Git commit<CR>", { desc = "Git commit" })
+		vim.keymap.set("n", "<leader>gC", ":Git commit -m ", { desc = "Git commit message" })
 
 		local klante_fugitive = vim.api.nvim_create_augroup("Klante_Fugitive", { clear = true })
 
@@ -10,15 +17,6 @@ return {
 			pattern = { "fugitive", "fugitiveblame" },
 			callback = function(args)
 				local opts = { buffer = args.buf, remap = false, silent = true }
-
-				vim.keymap.set("n", "<leader>p", function()
-					vim.cmd.Git("push")
-				end, vim.tbl_extend("force", opts, { desc = "Git push" }))
-
-				vim.keymap.set("n", "<leader>P", function()
-					vim.cmd.Git({ "pull", "--rebase" })
-				end, vim.tbl_extend("force", opts, { desc = "Git pull rebase" }))
-
 				vim.keymap.set("n", "<leader>gp", function()
 					vim.cmd.Git("push")
 				end, vim.tbl_extend("force", opts, { desc = "Git push" }))
--- nvim/lua/plugins/which-key.lua
+++ nvim/lua/plugins/which-key.lua
@@ -6,6 +6,10 @@ return {
 
 		wk.setup({})
 
+		vim.keymap.set("n", "<leader>g?", function()
+			wk.show({ keys = "<leader>g", mode = "n" })
+		end, { desc = "Git keymaps" })
+
 		wk.add({
 			{ "<leader>a", desc = "Harpoon add file" },
 			{ "<leader>c", group = "Code" },
@@ -13,11 +17,16 @@ return {
 			{ "<leader>e", desc = "Line diagnostics" },
 			{ "<leader>f", desc = "Format" },
 			{ "<leader>g", group = "Git" },
+			{ "<leader>g?", desc = "Git keymaps" },
+			{ "<leader>ga", desc = "Git add (prompt)" },
+			{ "<leader>gA", desc = "Git add all" },
+			{ "<leader>gc", desc = "Git commit" },
+			{ "<leader>gC", desc = "Git commit message" },
 			{ "<leader>gp", desc = "Git push" },
 			{ "<leader>gP", desc = "Git pull rebase" },
 			{ "<leader>gs", desc = "Git status" },
 			{ "<leader>gt", desc = "Git push set upstream" },
-			{ "<leader>p", group = "Paste/Push" },
+			{ "<leader>p", group = "Paste" },
 			{ "<leader>pv", desc = "Project tree" },
 			{ "<leader>r", group = "Rename" },
 			{ "<leader>rn", desc = "Rename symbol" },
